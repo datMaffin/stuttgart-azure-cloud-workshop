f7c84e1f30589d49dc6144566602923a7e56a99b
Dominic Giebert <dominic.giebert@motius.de>
2019-08-08 12:12:30 +0200

    Example service bus

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
-import express from 'express'
-const app = express()
-const port = 8080 // default port to listen
+import { delay, OnError, ReceiveMode, ServiceBusClient, ServiceBusMessage } from '@azure/service-bus'
+import config from '../config.json'
 
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+async function main(): Promise<void> {
+    const client = ServiceBusClient.createFromConnectionString(config.connectionString)
+    await sendMessage(client, 'session-1', 'test')
+    await receiveMessages(client, 'session-1')
+}
 
-// start the Express server
-app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`server started at http://localhost:${port}`)
+async function sendMessage(sbClient: ServiceBusClient, sessionId: string, body: string): Promise<void> {
+    // https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/servicebus/service-bus/#key-concepts
+    const queueClient = sbClient.createQueueClient(config.queueName)
+    const sender = queueClient.createSender()
+
+    await sender.send({
+        body,
+        label: 'Test',
+        sessionId
+    })
+
+    await queueClient.close()
+}
+
+async function receiveMessages(sbClient: ServiceBusClient, sessionId: string): Promise<void> {
+    // https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/servicebus/service-bus/#receive-messages
+    const queueClient = sbClient.createQueueClient(config.queueName)
+    const receiver = queueClient.createReceiver(ReceiveMode.peekLock, { sessionId })
+
+    // https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/servicebus/service-bus/#register-message-handler
+    const onMessage = async (brokeredMessage: ServiceBusMessage) => {
+        console.log(`Received: ${brokeredMessage.sessionId} - ${brokeredMessage.body} `)
+    }
+    const onError: OnError = (err): void => {
+        console.log('>>>>> Error occurred: ', err)
+    }
+    receiver.registerMessageHandler(onMessage, onError)
+    await delay(5000)
+
+    await queueClient.close()
+}
+
+main().catch(err => {
+    console.log('Error occurred: ', err)
 })
