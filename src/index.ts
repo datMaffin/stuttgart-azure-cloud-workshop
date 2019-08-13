56132c949fcb6c937f945e79a18441bd34006bfb
Marvin Dostal <maffinmaffinmaffinmaffin@gmail.com>
2019-08-13 15:19:20 +0200

    Implement simple bike post request

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import express from 'express'
-const app = express()
+import * as bodyparser from 'body-parser'
+import express, { NextFunction, Request, Response } from 'express'
+
 const port = 8080
 
+const loggerMiddleware = (request: Request, _response: Response, next: NextFunction) => {
+    console.log(`${request.method} ${request.path}`)
+    next()
+}
+
+const app = express()
+app.use(loggerMiddleware)
+app.use(bodyparser.json())
+
 // define a route handler for the default home page
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+app.post('/', (request: Request, response: Response) => {
+    if (request.body.bike === undefined) {
+        response.status(400).send('Not a bike')
+    } else {
+        response.status(200).json(request.body.bike)
+    }
 })
 
 // start the Express server
